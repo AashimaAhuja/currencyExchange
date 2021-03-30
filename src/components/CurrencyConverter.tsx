43bffccbb69c05e347fc510f96379b1f8fe42140
Aashima Ahuja <aashima.ahuja@booking.com>
2021-03-31 01:35:52 +0200

    Added styles

--- src/components/CurrencyConverter.tsx
+++ src/components/CurrencyConverter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const CurrencyConverter = (props: any) => (
   <div
+    className="currency-converter"
     style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}
   >
     <h1 className="margin-top--16"> Currency Converter</h1>
